4dc7662f4a0c625cc792688d089d0920572d6a6f
paraigor <103633539+paraigor@users.noreply.github.com>
2024-11-21 20:55:10 +0300

    Add data validation for register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -10,60 +10,73 @@ from .models import Order, OrderItem, Product
 
 def banners_list_api(request):
     # FIXME move data to db?
-    return JsonResponse([
-        {
-            'title': 'Burger',
-            'src': static('burger.jpg'),
-            'text': 'Tasty Burger at your door step',
+    return JsonResponse(
+        [
+            {
+                "title": "Burger",
+                "src": static("burger.jpg"),
+                "text": "Tasty Burger at your door step",
+            },
+            {
+                "title": "Spices",
+                "src": static("food.jpg"),
+                "text": "All Cuisines",
+            },
+            {
+                "title": "New York",
+                "src": static("tasty.jpg"),
+                "text": "Food is incomplete without a tasty dessert",
+            },
+        ],
+        safe=False,
+        json_dumps_params={
+            "ensure_ascii": False,
+            "indent": 4,
         },
-        {
-            'title': 'Spices',
-            'src': static('food.jpg'),
-            'text': 'All Cuisines',
-        },
-        {
-            'title': 'New York',
-            'src': static('tasty.jpg'),
-            'text': 'Food is incomplete without a tasty dessert',
-        }
-    ], safe=False, json_dumps_params={
-        'ensure_ascii': False,
-        'indent': 4,
-    })
+    )
 
 
 def product_list_api(request):
-    products = Product.objects.select_related('category').available()
+    products = Product.objects.select_related("category").available()
 
     dumped_products = []
     for product in products:
         dumped_product = {
-            'id': product.id,
-            'name': product.name,
-            'price': product.price,
-            'special_status': product.special_status,
-            'description': product.description,
-            'category': {
-                'id': product.category.id,
-                'name': product.category.name,
-            } if product.category else None,
-            'image': product.image.url,
-            'restaurant': {
-                'id': product.id,
-                'name': product.name,
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "special_status": product.special_status,
+            "description": product.description,
+            "category": {
+                "id": product.category.id,
+                "name": product.category.name,
             }
+            if product.category
+            else None,
+            "image": product.image.url,
+            "restaurant": {
+                "id": product.id,
+                "name": product.name,
+            },
         }
         dumped_products.append(dumped_product)
-    return JsonResponse(dumped_products, safe=False, json_dumps_params={
-        'ensure_ascii': False,
-        'indent': 4,
-    })
+    return JsonResponse(
+        dumped_products,
+        safe=False,
+        json_dumps_params={
+            "ensure_ascii": False,
+            "indent": 4,
+        },
+    )
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def register_order(request):
-    # TODO это лишь заглушка
     site_order = request.data
+    if not site_order.get("products") or not isinstance(
+        site_order["products"], list
+    ):
+        return Response({"error": "products not present or not a list"})
     order = Order.objects.create(
         first_name=site_order["firstname"],
         last_name=site_order["lastname"],
@@ -75,9 +88,9 @@ def register_order(request):
         order_product_id = order_product["product"]
         product = Product.objects.get(pk=order_product_id)
         OrderItem.objects.create(
-            order = order,
-            product = product,
-            quantity = order_product["quantity"],
+            order=order,
+            product=product,
+            quantity=order_product["quantity"],
         )
 
     return Response(site_order)
